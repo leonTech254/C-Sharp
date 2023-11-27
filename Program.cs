7ee20fa0d47069eddca1e66c92e5a4ca2ca6147b
leonTech254 <martinleontech23@gmail.com>
2023-11-27 11:34:18 +0300

    -classes & objects

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace HelloC
     {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
             Console.WriteLine("Hello leon");
 //changing console color
---
+++ classes_object.cs
@@ -0,0 +1,9 @@
+using System;
+public class Students
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello world");
+    }
+    
+}
